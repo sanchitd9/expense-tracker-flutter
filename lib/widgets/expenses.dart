11cab86f121eb9904e6ab61d314f2b2386cef7e1
Sanchit Dass <sanchitd9@gmail.com>
2024-06-04 00:05:50 -0400

    Set an overall theme

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/expenses_list/new_expense.dart';
 import 'package:flutter/material.dart';
